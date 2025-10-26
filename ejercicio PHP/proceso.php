976024eea3a50c31a47cc7644714917dcd838b1d
jch0020-Jorge <jch0020@alu.medac.es>
2025-10-26 23:28:22 +0100

    update

---
+++ ejercicio PHP/proceso.php
@@ -0,0 +1,16 @@
+<?php
+$nombre= $_GET[nombre];
+$apellidos= $_GET[apellidos];
+$edad= $_GET[edad];
+
+function comprobarEdad($nombre, $apellidos, $edad){
+    if ($edad >= 18) {
+        echo "Hola $nombre $apellidos, tienes $edad años y eres mayor de edad"
+    } else {
+        echo "Hola $nombre $apellidos, tienes $edad años y eres menor de edad"
+    }
+}
+
+
+comprobarEdad($nombre, $apellidos, $edad);
+?>
